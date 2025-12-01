ace7ec717344393c70c7dd663e06665c76113b95
krloves <karen.lovedorial@gmail.com>
2025-12-01 22:32:16 +0800

    delete files

--- site/main.py
+++ site/main.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+# app = Flask(__name__)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=7777)
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=7777)
