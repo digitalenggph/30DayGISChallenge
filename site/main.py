b0b8544e05c21bac4c1a14d42450ddf1bb94320e
krloves <karen.lovedorial@gmail.com>
2025-12-02 15:08:44 +0800

    use list of dictionaries instead of dataframe as it is easier to iterate through

--- site/main.py
+++ site/main.py
@@ -1,22 +1,27 @@
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 
-# Load Excel
-df = pd.read_excel("30DayGISChallenge.xlsx")  # replace with your file path
 
-# Prepare data
-columns = df.columns.tolist()
-rows = df.values.tolist()
+# Load the Excel file (one sheet for now hehe)
+data_df = pd.read_excel("30DayGISChallenge.xlsx")
+data = data_df.to_dict('records')
+
+# print(data[0].keys())
+# exit()
+
+context = {
+    'title': "#30DayMapChallenge2025",
+    'data': data
+    }
 
 # Setup Jinja environment
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('body.html')
 
-# Render the template with data
-output = template.render(columns=columns, rows=rows)
+rendered_html = template.render(context)
 
 # Save to HTML
 with open("output.html", "w", encoding="utf-8") as f:
-    f.write(output)
+    f.write(rendered_html)
 
 print("HTML file created successfully!")
